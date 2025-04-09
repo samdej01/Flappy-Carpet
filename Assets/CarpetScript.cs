a93f7d1649b4dfadbde2029421117165723c4a63
Samia Aldejwi <123486948+samdej01@users.noreply.github.com>
2025-04-09 13:30:39 +0300

    Game Over

--- Assets/CarpetScript.cs
+++ Assets/CarpetScript.cs
@@ -6,18 +6,25 @@ public class CarpetScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool Alive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && Alive == true)
 
         myRigidbody.linearVelocity = Vector2.up * flapStrength;  
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        Alive = false;
     }
 }
